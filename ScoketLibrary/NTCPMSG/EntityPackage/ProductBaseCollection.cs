f0dab6034c4dae8ecd3f9262565b3750fe607e73
chenwenguang <10217760@qq.com>
2017-12-16 23:44:50 +0800

    构建商品集合类

---
+++ ScoketLibrary/NTCPMSG/EntityPackage/ProductBaseCollection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTCPMessage.EntityPackage
+{
+    /// <summary>
+    /// 产品集合类
+    /// </summary>
+    public class ProductBaseCollection:List<ProductBase>
+    {
+    }
+}
