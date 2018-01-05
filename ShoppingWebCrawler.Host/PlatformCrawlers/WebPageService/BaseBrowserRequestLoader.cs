2f26405ccfa38d32e89e1a99ae49b2419cde4d73
chenwenguang <c-wenguang@kuaiyixing.com.cn>
2018-01-05 19:33:18 +0800

    自动刷新browser 时间缩短

--- ShoppingWebCrawler.Host/PlatformCrawlers/WebPageService/BaseBrowserRequestLoader.cs
+++ ShoppingWebCrawler.Host/PlatformCrawlers/WebPageService/BaseBrowserRequestLoader.cs
@@ -213,7 +213,7 @@ namespace ShoppingWebCrawler.Host.PlatformCrawlers.WebPageService
 
             this.LoadUrlGetContentByCefBrowser(refreshCookieUrl);
             //不定时刷新--时间段在redis cookie  过期之间，redis 过期为5 min
-            this.NextUpdateCookieTime = DateTime.Now.AddSeconds(new Random().Next(10, 60));
+            this.NextUpdateCookieTime = DateTime.Now.AddSeconds(new Random().Next(20, 90));
         }
 
         /// <summary>
